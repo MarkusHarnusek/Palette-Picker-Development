43aefde637fd5874409a56ca305eb79c38733739
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-05-12 12:25:46 +0200

    Added translation for options window and error message boxes in the Save.cs file

--- OptionsWindow.xaml.cs
+++ OptionsWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PalettePicker
 {
@@ -19,9 +7,144 @@ namespace PalettePicker
     /// </summary>
     public partial class OptionsWindow : Window
     {
+        int language = 0;
+
         public OptionsWindow()
         {
             InitializeComponent();
+        }
+
+        private void SetLanguage(int languageID)
+        {
+            switch (languageID)
+            {
+                case 0:
+                    Txt_WindowTitle.Text = "Options";
+                    Btn_Preferences.Content = "Preferences";
+                    Txt_Preferences_Title.Text = "Preferences";
+                    Txt_Preferences_Language_Title.Text = "Language";
+                    Btn_Apply.Content = "Apply";
+
+                    break;
+
+                case 1:
+                    Txt_WindowTitle.Text = "Einstellungen";
+                    Btn_Preferences.Content = "Präferenzen";
+                    Txt_Preferences_Title.Text = "Präferenzen";
+                    Txt_Preferences_Language_Title.Text = "Sprache";
+                    Btn_Apply.Content = "Anwenden";
+
+                    break;
+
+                case 2:
+                    Txt_WindowTitle.Text = "Ajustes";
+                    Btn_Preferences.Content = "Ajustes";
+                    Txt_Preferences_Title.Text = "Ajustes";
+                    Txt_Preferences_Language_Title.Text = "Idioma";
+                    Btn_Apply.Content = "Aplicar";
+
+                    break;
+
+                case 3:
+                    Txt_WindowTitle.Text = "Paramètres";
+                    Btn_Preferences.Content = "Preferencias";
+                    Txt_Preferences_Title.Text = "Préférences";
+                    Txt_Preferences_Language_Title.Text = "Langue";
+                    Btn_Apply.Content = "Appliquer";
+
+                    break;
+
+                case 4:
+                    Txt_WindowTitle.Text = "设置";
+                    Btn_Preferences.Content = "首选项";
+                    Txt_Preferences_Title.Text = "首选项";
+                    Txt_Preferences_Language_Title.Text = "语言";
+                    Btn_Apply.Content = "应用";
+
+                    break;
+
+                case 5:
+                    Txt_WindowTitle.Text = "Configurações";
+                    Btn_Preferences.Content = "Preferências";
+                    Txt_Preferences_Title.Text = "Preferências";
+                    Txt_Preferences_Language_Title.Text = "Idioma";
+                    Btn_Apply.Content = "Aplicar";
+
+                    break;
+
+                case 6:
+                    Txt_WindowTitle.Text = "Настройки";
+                    Btn_Preferences.Content = "Предпочтения";
+                    Txt_Preferences_Title.Text = "Предпочтения";
+                    Txt_Preferences_Language_Title.Text = "Язык";
+                    Btn_Apply.Content = "Применить";
+
+                    break;
+            }
+        }
+
+        private (string msg, string title) GetTranslatedEmptyLanguageSelectionErrorMessage(int languageId)
+        {
+            if (Lst_Language.SelectedItem == null)
+            {
+                string msg = string.Empty;
+                string title = string.Empty;
+
+                switch (languageId)
+                {
+                    case 0:
+                        msg = "Please select a language.";
+                        title = "Error";
+                        break;
+
+                    case 1:
+                        msg = "Bitte wählen Sie eine Sprache aus.";
+                        title = "Fehler";
+                        break;
+
+                    case 2:
+                        msg = "Por favor, seleccione un idioma.";
+                        title = "Error";
+                        break;
+
+                    case 3:
+                        msg = "Veuillez sélectionner une langue.";
+                        title = "Erreur";
+                        break;
+
+                    case 4:
+                        msg = "请选择一种语言。";
+                        title = "错误";
+                        break;
+
+                    case 5:
+                        msg = "Por favor, selecione um idioma.";
+                        title = "Erro";
+                        break;
+
+                    case 6:
+                        msg = "Пожалуйста, выберите язык.";
+                        title = "Ошибка";
+                        break;
+                }
+
+                return (msg, title);
+            }
+
+            return (string.Empty, string.Empty);
+        }
+
+        private void Btn_Apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (Lst_Language.SelectedItem == null)
+            {
+                MessageBox.Show(GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).msg, GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).title, MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MainWindow.currentLanguage = Lst_Language.SelectedIndex;
+                SetLanguage(MainWindow.currentLanguage);
+            }
         }
     }
 }
--- Save.cs
+++ Save.cs
@@ -7,12 +7,227 @@ namespace PalettePicker
     {
         private static string filePath = string.Empty;
 
+        #region Translation
+
+        private static (string msg, string title) GetTranslatedInvalidErrMsg(int msgId, int languageId)
+        {
+            switch (msgId)
+            {
+                case 0:
+                    switch (languageId)
+                    {
+                        case 0: return ("No file selected.", "Select Error");
+                        case 1: return ("Keine Datei ausgewählt.", "Fehler beim Auswählen");
+                        case 2: return ("Ningún archivo seleccionado.", "Seleccionar error");
+                        case 3: return ("Aucun fichier sélectionné.", "Erreur de sélection");
+                        case 4: return ("未选择文件。", "选择错误");
+                        case 5: return ("Nenhum arquivo selecionado.", "Erro de seleção");
+                        case 6: return ("Не выбрана ни одна файл.", "Ошибка выбора");
+                    }
+
+                    break;
+
+                case 1:
+                    switch (languageId)
+                    {
+                        case 0: return ("The selected file is not a valid palette JSON file.", "File Error");
+                        case 1: return ("Die ausgewählte Datei ist keine gültige Palette-JSON-Datei.", "Dateifehler");
+                        case 2: return ("El archivo seleccionado no es un archivo JSON de paleta válido.", "Error de archivo");
+                        case 3: return ("Le fichier sélectionné n'est pas un fichier JSON de palette valide.", "Erreur de fichier");
+                        case 4: return ("所选文件不是有效的调色板 JSON 文件。", "文件错误");
+                        case 5: return ("O arquivo selecionado não é um arquivo JSON de paleta válido.", "Erro de arquivo");
+                        case 6: return ("Выбранный файл не является допустимым файлом JSON палитры.", "Ошибка файла");
+                    }
+
+                    break;
+
+                case 2:
+                    switch (languageId)
+                    {
+                        case 0: return ("No folder select.", "Select Error");
+                        case 1: return ("Kein Ordner ausgewählt.", "Fehler bei der Auswahl");
+                        case 2: return ("Ninguna carpeta seleccionada.", "Error de selección");
+                        case 3: return ("Aucun dossier sélectionné.", "Erreur de sélection");
+                        case 4: return ("未选择文件夹。", "选择错误");
+                        case 5: return ("Nenhuma pasta selecionada.", "Erro de seleção");
+                        case 6: return ("Не выбрана ни одна папка.", "Ошибка выбора");
+                    }
+
+                    break;
+
+                case 3:
+                    switch (languageId)
+                    {
+                        case 0: return ("Error occured while trying to save file. Path is either empty or does not exist.", "Save Error");
+                        case 1: return ("Fehler beim Speichern der Datei. Der Pfad ist entweder leer oder existiert nicht.", "Speicher Fehler");
+                        case 2: return ("Se produjo un error al intentar guardar el archivo. La ruta está vacía o no existe.", "Error de guardado");
+                        case 3: return ("Une erreur s'est produite lors de la tentative d'enregistrement du fichier. Le chemin est vide ou n'existe pas.", "Erreur d'enregistrement");
+                        case 4: return ("保存文件时发生错误。路径为空或不存在。", "保存错误");
+                        case 5: return ("Ocorreu um erro ao tentar salvar o arquivo. O caminho está vazio ou não existe.", "Erro de salvamento");
+                        case 6: return ("Произошла ошибка при попытке сохранить файл. Путь либо пуст, либо не существует.", "Ошибка сохранения");
+                    }
+
+                    break;
+
+                case 4:
+                    switch (languageId)
+                    {
+                        case 0: return ("Error occured while extracting json save file with following error message:", "File Read Error");
+                        case 1: return ("Fehler beim Extrahieren der JSON-Speicherdatei mit folgender Fehlermeldung:", "Dateilesefehler");
+                        case 2: return ("Se produjo un error al extraer el archivo de guardado JSON con el siguiente mensaje de error:", "Error de lectura de archivo");
+                        case 3: return ("Une erreur s'est produite lors de l'extraction du fichier de sauvegarde JSON avec le message d'erreur suivant:", "Erreur de lecture de fichier");
+                        case 4: return ("提取 JSON 保存文件时发生错误，错误消息如下：", "文件读取错误");
+                        case 5: return ("Ocorreu um erro ao extrair o arquivo de salvamento JSON com a seguinte mensagem de erro:", "Erro de leitura de arquivo");
+                        case 6: return ("Произошла ошибка при извлечении файла сохранения JSON со следующим сообщением об ошибке:", "Ошибка чтения файла");
+                    }
+
+                    break;
+
+                case 5:
+                    switch (languageId)
+                    {
+                        case 0: return ("Error occured while extracting json save file.", "File Read Error");
+                        case 1: return ("Fehler beim Extrahieren der JSON-Speicherdatei.", "Dateilesefehler");
+                        case 2: return ("Se produjo un error al extraer el archivo de guardado JSON.", "Error de lectura de archivo");
+                        case 3: return ("Une erreur s'est produite lors de l'extraction du fichier de sauvegarde JSON.", "Erreur de lecture de fichier");
+                        case 4: return ("提取 JSON 保存文件时发生错误。", "文件读取错误");
+                        case 5: return ("Ocorreu um erro ao extrair o arquivo de salvamento JSON.", "Erro de leitura de arquivo");
+                        case 6: return ("Произошла ошибка при извлечении файла сохранения JSON.", "Ошибка чтения файла");
+                    }
+
+                    break;
+            }
+
+            return (string.Empty, string.Empty);
+        }
+
+        private static string GetShortTranslatedErrMsg(int errorId, int languageId)
+        {
+            switch (errorId)
+            {
+                case 0:
+                    switch (languageId)
+                    {
+                        case 0: return "File does not exist.";
+                        case 1: return "Datei existiert nicht.";
+                        case 2: return "El archivo no existe.";
+                        case 3: return "Le fichier n'existe pas.";
+                        case 4: return "文件不存在。";
+                        case 5: return "O arquivo não existe.";
+                        case 6: return "Файл не существует.";
+                    }
+
+                    break;
+
+                case 1:
+                    switch (languageId)
+                    {
+                        case 0: return "File is not a JSON file.";
+                        case 1: return "Datei ist keine JSON-Datei.";
+                        case 2: return "El archivo no es un archivo JSON.";
+                        case 3: return "Le fichier n'est pas un fichier JSON.";
+                        case 4: return "文件不是 JSON 文件。";
+                        case 5: return "O arquivo não é um arquivo JSON.";
+                        case 6: return "Файл не является файлом JSON.";
+                    }
+
+                    break;
+
+                case 2:
+                    switch (languageId)
+                    {
+                        case 0: return "File is empty.";
+                        case 1: return "Datei ist leer.";
+                        case 2: return "El archivo está vacío.";
+                        case 3: return "Le fichier est vide.";
+                        case 4: return "文件是空的。";
+                        case 5: return "O arquivo está vazio.";
+                        case 6: return "Файл пуст.";
+                    }
+
+                    break;
+
+                case 3:
+                    switch (languageId)
+                    {
+                        case 0: return "JSON data was null.";
+                        case 1: return "JSON-Daten waren null.";
+                        case 2: return "Los datos JSON eran nulos.";
+                        case 3: return "Les données JSON étaient nulles.";
+                        case 4: return "JSON 数据为 null。";
+                        case 5: return "Os dados JSON eram nulos.";
+                        case 6: return "Данные JSON были нулевыми.";
+                    }
+
+                    break;
+
+                case 4:
+                    switch (languageId)
+                    {
+                        case 0: return "File does not contain the correct number of value pairs.";
+                        case 1: return "Datei enthält nicht die richtige Anzahl von Wertpaaren.";
+                        case 2: return "El archivo no contiene el número correcto de pares de valores.";
+                        case 3: return "Le fichier ne contient pas le bon nombre de paires de valeurs.";
+                        case 4: return "文件不包含正确数量的值对。";
+                        case 5: return "O arquivo não contém o número correto de pares de valores.";
+                        case 6: return "Файл не содержит правильного количества пар значений.";
+                    }
+
+                    break;
+
+                case 5:
+                    switch (languageId)
+                    {
+                        case 0: return "Missing required key";
+                        case 1: return "Fehlender erforderlicher Schlüssel";
+                        case 2: return "Falta la clave requerida";
+                        case 3: return "Clé requise manquante";
+                        case 4: return "缺少必需的密钥";
+                        case 5: return "Falta a chave obrigatória";
+                        case 6: return "Отсутствует обязательный ключ";
+                    }
+
+                    break;
+
+                case 6:
+                    switch (languageId)
+                    {
+                        case 0: return "Error reading file";
+                        case 1: return "Fehler beim Lesen der Datei";
+                        case 2: return "Error al leer el archivo";
+                        case 3: return "Erreur de lecture du fichier";
+                        case 4: return "读取文件时出错";
+                        case 5: return "Erro ao ler o arquivo";
+                        case 6: return "Ошибка чтения файла";
+                    }
+
+                    break;
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+
         public static void SelectFile()
         {
+            string title = string.Empty;
+
+            switch (MainWindow.currentLanguage)
+            {
+                case 0: title = "Select the color palette"; break;
+                case 1: title = "Wählen Sie die Farbpalette aus"; break;
+                case 2: title = "Seleccione la paleta de colores"; break;
+                case 3: title = "Sélectionnez la palette de couleurs"; break;
+                case 4: title = "选择调色板"; break;
+                case 5: title = "Selecione a paleta de cores"; break;
+                case 6: title = "Выберите цветовую палитру"; break;
+            }
+
             Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
             {
                 Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*",
-                Title = "Select the color palette",
+                Title = title,
                 CheckFileExists = true,
                 Multiselect = false,
                 CheckPathExists = true
@@ -25,13 +240,13 @@ namespace PalettePicker
 
             if (string.IsNullOrEmpty(filePath))
             {
-                System.Windows.MessageBox.Show("No file selected.", "Select Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show(GetTranslatedInvalidErrMsg(0, MainWindow.currentLanguage).msg, GetTranslatedInvalidErrMsg(0, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return;
             }
 
             if (!ValidatePallleteJsonFile(filePath, out string errMsg))
             {
-                System.Windows.MessageBox.Show($"The selected file is not a valid palette JSON file. {errMsg}", "Select Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show($"{GetTranslatedInvalidErrMsg(1, MainWindow.currentLanguage).msg} {errMsg}", GetTranslatedInvalidErrMsg(1, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return;
             }
 
@@ -53,7 +268,7 @@ namespace PalettePicker
             }
             else
             {
-                System.Windows.MessageBox.Show("No folder selected.", "Select Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show(GetTranslatedInvalidErrMsg(2, MainWindow.currentLanguage).msg, GetTranslatedInvalidErrMsg(2, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return string.Empty;
             }
         }
@@ -90,7 +305,7 @@ namespace PalettePicker
             }
             else
             {
-                System.Windows.MessageBox.Show("Error occured while trying to save file. Path is either empty or does not exist.", "Save Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show(GetTranslatedInvalidErrMsg(3, MainWindow.currentLanguage).msg, GetTranslatedInvalidErrMsg(3, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
             }
         }
 
@@ -98,14 +313,14 @@ namespace PalettePicker
         {
             if (!ValidatePallleteJsonFile(path, out string errMsg))
             {
-                System.Windows.MessageBox.Show($"Error occured while extracting json save file with following error message: {errMsg}", "File Read Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show($"{GetTranslatedInvalidErrMsg(4, MainWindow.currentLanguage).msg} {errMsg}", GetTranslatedInvalidErrMsg(4, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return (string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, false, false, false);
             }
 
             Dictionary<string, string>? jsonData = GetJsonValues(path);
             if (jsonData == null)
             {
-                System.Windows.MessageBox.Show("Error occured while extracting json save file.", "File Read Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show(GetTranslatedInvalidErrMsg(5, MainWindow.currentLanguage).msg, GetTranslatedInvalidErrMsg(5, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return (string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, false, false, false);
             }
 
@@ -126,7 +341,7 @@ namespace PalettePicker
         {
             if (!ValidatePallleteJsonFile(path, out string errMsg))
             {
-                System.Windows.MessageBox.Show($"Error occured while extracting json save file with following error message: {errMsg}", "File Read Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                System.Windows.MessageBox.Show($"{GetTranslatedInvalidErrMsg(4, MainWindow.currentLanguage).msg} {errMsg}", GetTranslatedInvalidErrMsg(4, MainWindow.currentLanguage).title, System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
                 return null;
             }
 
@@ -153,24 +368,24 @@ namespace PalettePicker
         {
             if (!File.Exists(jsonFile))
             {
-                errorMessage = "File does not exist.";
+                errorMessage = GetShortTranslatedErrMsg(0, MainWindow.currentLanguage);
                 return false;
             }
 
             string fileExtension = Path.GetExtension(jsonFile);
             if (fileExtension != ".json")
             {
-                errorMessage = "File is not a JSON file.";
+                errorMessage = GetShortTranslatedErrMsg(1, MainWindow.currentLanguage);
                 return false;
             }
 
             try
             {
-                string content = File.ReadAllText(jsonFile);
+                string? content = File.ReadAllText(jsonFile);
 
                 if (string.IsNullOrWhiteSpace(content))
                 {
-                    errorMessage = "File is empty.";
+                    errorMessage = GetShortTranslatedErrMsg(2, MainWindow.currentLanguage);
                     return false;
                 }
 
@@ -178,12 +393,12 @@ namespace PalettePicker
 
                 if (jsonData == null)
                 {
-                    errorMessage = "JSON data was null.";
+                    errorMessage = GetShortTranslatedErrMsg(3, MainWindow.currentLanguage);
                 }
 
                 if (jsonData.Count != 11)
                 {
-                    errorMessage = "File does not contain the correct number of value pairs.";
+                    errorMessage = GetShortTranslatedErrMsg(4, MainWindow.currentLanguage);
                     return false;
                 }
 
@@ -192,7 +407,7 @@ namespace PalettePicker
                 {
                     if (!jsonData.ContainsKey(key))
                     {
-                        errorMessage = $"Missing required key: {key}";
+                        errorMessage = $"{GetShortTranslatedErrMsg(5, MainWindow.currentLanguage)}: {key}";
                         return false;
                     }
                 }
@@ -202,7 +417,7 @@ namespace PalettePicker
             }
             catch (Exception ex)
             {
-                errorMessage = $"Error reading file: {ex.Message}";
+                errorMessage = $"{GetShortTranslatedErrMsg(6, MainWindow.currentLanguage)}: {ex.Message}";
                 return false;
             }
         }
