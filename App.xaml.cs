8957236e77e08cd856ab6ee17c18d2514fb1d5bd
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-06-05 10:29:06 +0200

    Changed from using Static Window Resources to App wide Dynamic Resource

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+﻿using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Media;
 
 namespace PalettePicker
 {
@@ -9,6 +11,28 @@ namespace PalettePicker
     /// </summary>
     public partial class App : Application
     {
-    }
+        public void SetDarkTheme()
+        {
+            Application.Current.Resources["P1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#070d0d"));
+            Application.Current.Resources["P2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#002147"));
+            Application.Current.Resources["S1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0055bf"));
+            Application.Current.Resources["S2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#87cefa"));
+            Application.Current.Resources["T"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#eefafb"));
+        }
 
+        public void SetLightTheme()
+        {
+            Application.Current.Resources["P1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#eefafb"));
+            Application.Current.Resources["P2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#87cefa"));
+            Application.Current.Resources["S1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0055bf"));
+            Application.Current.Resources["S2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#002147"));
+            Application.Current.Resources["T"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#070d0d"));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
