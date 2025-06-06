3ff23492cc4eeb1d03aa3f5b1bfc901ba89bb024
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-06-06 08:12:06 +0200

    Removed language select combobox

--- OptionsWindow.xaml.cs
+++ OptionsWindow.xaml.cs
@@ -24,8 +24,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Options";
                     Btn_Language.Content = "Language";
                     Txt_Language_Title.Text = "Language";
-                    Txt_Language_Language_Title.Text = "Language";
-                    Btn_Apply.Content = "Apply";
                     this.Title = "Options";
 
                     break;
@@ -34,8 +32,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Einstellungen";
                     Btn_Language.Content = "Sprache";
                     Txt_Language_Title.Text = "Sprache";
-                    Txt_Language_Language_Title.Text = "Sprache";
-                    Btn_Apply.Content = "Anwenden";
                     this.Title = "Einstellungen";
 
                     break;
@@ -44,8 +40,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Ajustes";
                     Btn_Language.Content = "Idioma";
                     Txt_Language_Title.Text = "Idioma";
-                    Txt_Language_Language_Title.Text = "Idioma";
-                    Btn_Apply.Content = "Aplicar";
                     this.Title = "Ajustes";
 
                     break;
@@ -54,8 +48,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Paramètres";
                     Btn_Language.Content = "Langue";
                     Txt_Language_Title.Text = "Langue";
-                    Txt_Language_Language_Title.Text = "Langue";
-                    Btn_Apply.Content = "Appliquer";
                     this.Title = "Paramètres";
 
                     break;
@@ -64,8 +56,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "设置";
                     Btn_Language.Content = "语言";
                     Txt_Language_Title.Text = "语言";
-                    Txt_Language_Language_Title.Text = "语言";
-                    Btn_Apply.Content = "应用";
                     this.Title = "设置";
 
                     break;
@@ -74,8 +64,6 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Configurações";
                     Btn_Language.Content = "Idioma";
                     Txt_Language_Title.Text = "Idioma";
-                    Txt_Language_Language_Title.Text = "Idioma";
-                    Btn_Apply.Content = "Aplicar";
                     this.Title = "Configurações";
 
                     break;
@@ -84,98 +72,93 @@ namespace PalettePicker
                     Txt_WindowTitle.Text = "Настройки";
                     Btn_Language.Content = "Язык";
                     Txt_Language_Title.Text = "Язык";
-                    Txt_Language_Language_Title.Text = "Язык";
-                    Btn_Apply.Content = "Применить";
                     this.Title = "Настройки";
 
                     break;
             }
         }
 
-        private (string msg, string title) GetTranslatedEmptyLanguageSelectionErrorMessage(int languageId)
-        {
-            if (Lst_Language.SelectedItem == null)
-            {
-                string msg = string.Empty;
-                string title = string.Empty;
+        //private (string msg, string title) GetTranslatedEmptyLanguageSelectionErrorMessage(int languageId)
+        //{
+        //    if (Lst_Language.SelectedItem == null)
+        //    {
+        //        string msg = string.Empty;
+        //        string title = string.Empty;
 
-                switch (languageId)
-                {
-                    case 0:
-                        msg = "Please select a language.";
-                        title = "Error";
-                        break;
+        //        switch (languageId)
+        //        {
+        //            case 0:
+        //                msg = "Please select a language.";
+        //                title = "Error";
+        //                break;
 
-                    case 1:
-                        msg = "Bitte wählen Sie eine Sprache aus.";
-                        title = "Fehler";
-                        break;
+        //            case 1:
+        //                msg = "Bitte wählen Sie eine Sprache aus.";
+        //                title = "Fehler";
+        //                break;
 
-                    case 2:
-                        msg = "Por favor, seleccione un idioma.";
-                        title = "Error";
-                        break;
+        //            case 2:
+        //                msg = "Por favor, seleccione un idioma.";
+        //                title = "Error";
+        //                break;
 
-                    case 3:
-                        msg = "Veuillez sélectionner une langue.";
-                        title = "Erreur";
-                        break;
+        //            case 3:
+        //                msg = "Veuillez sélectionner une langue.";
+        //                title = "Erreur";
+        //                break;
 
-                    case 4:
-                        msg = "请选择一种语言。";
-                        title = "错误";
-                        break;
+        //            case 4:
+        //                msg = "请选择一种语言。";
+        //                title = "错误";
+        //                break;
 
-                    case 5:
-                        msg = "Por favor, selecione um idioma.";
-                        title = "Erro";
-                        break;
+        //            case 5:
+        //                msg = "Por favor, selecione um idioma.";
+        //                title = "Erro";
+        //                break;
 
-                    case 6:
-                        msg = "Пожалуйста, выберите язык.";
-                        title = "Ошибка";
-                        break;
-                }
+        //            case 6:
+        //                msg = "Пожалуйста, выберите язык.";
+        //                title = "Ошибка";
+        //                break;
+        //        }
 
-                return (msg, title);
-            }
+        //        return (msg, title);
+        //    }
 
-            return (string.Empty, string.Empty);
-        }
+        //    return (string.Empty, string.Empty);
+        //}
 
-        private void Btn_Apply_Click(object sender, RoutedEventArgs e)
-        {
-            if (Lst_Language.SelectedItem == null)
-            {
-                MessageBox.Show(GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).msg, GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).title, MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            else
-            {
-                MainWindow.currentLanguage = Lst_Language.SelectedIndex;
-                SetLanguage(MainWindow.currentLanguage);
+        //private void Btn_Apply_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (Lst_Language.SelectedItem == null)
+        //    {
+        //        MessageBox.Show(GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).msg, GetTranslatedEmptyLanguageSelectionErrorMessage(MainWindow.currentLanguage).title, MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+        //    else
+        //    {
+        //        MainWindow.currentLanguage = Lst_Language.SelectedIndex;
+        //        SetLanguage(MainWindow.currentLanguage);
 
-                foreach (Window window in Application.Current.Windows)
-                {
-                    if (window is ColorPickerWindow colorPickerWindow)
-                    {
-                        colorPickerWindow.SetLanguage(MainWindow.currentLanguage, colorPickerWindow);
-                    }
-                    else if (window is MainWindow mainWindow)
-                    {
-                        MainWindow.SetLanguage(MainWindow.currentLanguage, mainWindow);
-                    }
-                }
+        //        foreach (Window window in Application.Current.Windows)
+        //        {
+        //            if (window is ColorPickerWindow colorPickerWindow)
+        //            {
+        //                colorPickerWindow.SetLanguage(MainWindow.currentLanguage, colorPickerWindow);
+        //            }
+        //            else if (window is MainWindow mainWindow)
+        //            {
+        //                MainWindow.SetLanguage(MainWindow.currentLanguage, mainWindow);
+        //            }
+        //        }
 
-            }
-        }
+        //    }
+        //}
 
         private void Brd_Btn_Preferences_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            //TODO fix animation
-
             if (sender is Border border)
             {
-                // Store the original height only once
                 if (border.Tag is not double)
                 {
                     border.Tag = border.Height;
@@ -197,8 +180,6 @@ namespace PalettePicker
 
         private void Brd_Btn_Preferences_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            //TODO fix animation            
-
             if (sender is Border border && border.Tag is double originalHeight)
             {
                 var animation = new System.Windows.Media.Animation.DoubleAnimation
