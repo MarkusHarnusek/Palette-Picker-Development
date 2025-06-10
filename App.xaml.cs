c04a24a1daec18be85104e3a85445c17bbfea450
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-06-10 11:26:04 +0200

    Added code grouping and documentation for the App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,23 +11,34 @@ namespace PalettePicker
     /// </summary>
     public partial class App : Application
     {
+
+        #region ThemeSetMethods
+
+        /// <summary>
+        /// Applies the dark theme to the application.
+        /// </summary>
         public void SetDarkTheme()
         {
             Application.Current.Resources["P1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#070d0d"));
-            Application.Current.Resources["P2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#002147"));
+            Application.Current.Resources["P2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#262626"));
             Application.Current.Resources["S1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0055bf"));
             Application.Current.Resources["S2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#87cefa"));
             Application.Current.Resources["T"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#eefafb"));
         }
 
+        /// <summary>
+        /// Applies the ligth theme to the application.
+        /// </summary>
         public void SetLightTheme()
         {
             Application.Current.Resources["P1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#eefafb"));
             Application.Current.Resources["P2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#87cefa"));
             Application.Current.Resources["S1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0055bf"));
-            Application.Current.Resources["S2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#002147"));
+            Application.Current.Resources["S2"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#262626"));
             Application.Current.Resources["T"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#070d0d"));
         }
+
+        #endregion
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
